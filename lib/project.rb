e0bfb003bc474052edec5a2d1eecbb80e93c822d
Kimberly Aviles <01010110.v@gmail.com>
2019-02-27 23:14:11 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   def add_backer(backer)
     self.backers << backer
     # backer.back_project(self) unless backer.backed_projects.include?(self)
-    if backer.backed_projects.!include?(self)
+    if !backer.backed_projects.include?(self)
       backer.back_project(self)
     end
   end
