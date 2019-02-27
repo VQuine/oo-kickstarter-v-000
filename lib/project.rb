7407652964b95ef501a831b7f1a6bc87cd03486b
Kimberly Aviles <01010110.v@gmail.com>
2019-02-27 23:18:15 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
 
   def add_backer(backer)
     self.backers << backer
-    backer.backed_projects.include?(self) ? nil : backer.back_project(self)
+    backer.backed_projects.include?(self) ? nil : backer.back_project(self) #<----- stops the infinite loop caused by the checking between classes
   end
 
 end #<-----CLASS end
